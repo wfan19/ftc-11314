602b76e6de628c905c3655841817fb45edf3386c
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-09-17 18:54:19 -0400

    More In-depth Sensor Implementation

--- ModernRoboticsI2cColorSensor.java
+++ ModernRoboticsI2cColorSensor.java
@@ -1,33 +1,3 @@
-/*
-        * Copyright (c) 2014, 2015 Qualcomm Technologies Inc
-        *
-        * All rights reserved.
-        *
-        * Redistribution and use in source and binary forms, with or without modification, are permitted
-        * (subject to the limitations in the disclaimer below) provided that the following conditions are
-        * met:
-        *
-        * Redistributions of source code must retain the above copyright notice, this list of conditions
-        * and the following disclaimer.
-        *
-        * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
-        * and the following disclaimer in the documentation and/or other materials provided with the
-        * distribution.
-        *
-        * Neither the name of Qualcomm Technologies Inc nor the names of its contributors may be used to
-        * endorse or promote products derived from this software without specific prior written permission.
-        *
-        * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS
-        * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-        * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-        * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-        * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-        * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-        * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-        * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-        * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        */
-
         package org.firstinspires.ftc.teamcode;
 
         import com.qualcomm.hardware.modernrobotics.ModernRoboticsUsbDeviceInterfaceModule;
@@ -43,10 +13,6 @@
 
 public class ModernRoboticsI2cColorSensor extends I2cControllerPortDeviceImpl implements ColorSensor, I2cController.I2cPortReadyCallback {
 
-    //------------------------------------------------------------------------------------------------
-    // Constants
-    //------------------------------------------------------------------------------------------------
-
     public final static I2cAddr DEFAULT_I2C_ADDRESS = I2cAddr.create8bit(0x3C);
     public final static int ADDRESS_COMMAND = 0x03;
     public final static int ADDRESS_COLOR_NUMBER = 0x04;
@@ -60,10 +26,6 @@ public class ModernRoboticsI2cColorSensor extends I2cControllerPortDeviceImpl im
     public final static int COMMAND_PASSIVE_LED = 0x01;
     public final static int COMMAND_ACTIVE_LED = 0x00;
 
-    //------------------------------------------------------------------------------------------------
-    // State
-    //------------------------------------------------------------------------------------------------
-
     private volatile I2cAddr i2cAddr = DEFAULT_I2C_ADDRESS; // this can be changed by the user
     private          byte[]  readBuffer;
     private          Lock    readLock;
@@ -73,10 +35,6 @@ public class ModernRoboticsI2cColorSensor extends I2cControllerPortDeviceImpl im
     private enum State {READING_ONLY, PERFORMING_WRITE, SWITCHING_TO_READ};
     private State state = State.READING_ONLY;
     private int lastCommand = COMMAND_ACTIVE_LED; // NXT Sensor starts up with the LED on.
-
-    //------------------------------------------------------------------------------------------------
-    // Construction
-    //------------------------------------------------------------------------------------------------
 
     public ModernRoboticsI2cColorSensor(I2cController module, int physicalPort) {
         super(module, physicalPort);
@@ -96,10 +54,6 @@ public class ModernRoboticsI2cColorSensor extends I2cControllerPortDeviceImpl im
 
         controller.registerForI2cPortReadyCallback(this, physicalPort);
     }
-
-    //------------------------------------------------------------------------------------------------
-    // Operations
-    //------------------------------------------------------------------------------------------------
 
     @Override
     public String toString() {
@@ -196,7 +150,7 @@ public class ModernRoboticsI2cColorSensor extends I2cControllerPortDeviceImpl im
 
     @Override
     public void close() {
-        // take no action
+
     }
 
     @Override
